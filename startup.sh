4baedfb67dcd42969f800d691f81680e437260fc
Andrei Poenaru <andrei.poenaru@gmail.com>
2017-06-22 22:46:32 +0100

    Remove any leftover apache run files before restarting.

--- startup.sh
+++ startup.sh
@@ -8,6 +8,10 @@ finish() {
 
 trap 'finish' SIGTERM
 
+# Remove any leftover apache2 run files just in case
+rm -rf /var/run/lock/apache2/
+rm -rf /var/run/apache2/
+
 service redis-server start && /linkr/check_db.sh && apache2ctl -D FOREGROUND &
 wait
 
